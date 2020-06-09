5b5a7fc9b4eec931852b29c7bd71067250a33976
nickupx <nick.upsky@gmail.com>
2020-06-09 21:38:24 +0300

    all tasks solved (but task 3 works dumb - year only, which is incorrect)

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,46 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+    let d = b * b - 4 * a * c;
+    let x, x1, x2;
+    if (d < 0) {
+        x = [];
+    } else if (d === 0) {
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x = [x1];
+    } else if (d > 0) {
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        x = [x1, x2];
+    }
+    return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    let sum, averageMark;
+    if (marks) {
+        if (marks.length > 5) {
+            console.log('Оценок больше пяти')
+        }
+        marks.splice(5, marks.length);
+        sum = 0;
+        for (let i = 0; i < marks.length; i++) {
+            sum += marks[i];
+        }
+        averageMark = sum / marks.length;
+    }
+    if (isNaN(averageMark) == true) {
+        averageMark = 0;
+    }
+    return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+    let today = new Date();
+    let yearToday = today.getFullYear();
+    let yearOfBirth = dateOfBirthday.getFullYear();
+    if (yearToday - yearOfBirth >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
+    }
+    return result;
 }
