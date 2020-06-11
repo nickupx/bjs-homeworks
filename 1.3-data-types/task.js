ea7e63794945ffb0441b04e258996ec7761b1597
nickupx <nick.upsky@gmail.com>
2020-06-12 00:38:58 +0300

    tasks solved

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,44 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    percent = parseFloat(percent);
+    contribution = parseFloat(contribution);
+    amount = parseFloat(amount);
+    let totalAmount;
+    let input = [
+        ['Процентная ставка', 'Начальный взнос', 'Общая стоимость', 'Срок ипотеки'],
+        [percent, contribution, amount, date],
+    ];
+    let errorFields = [];
+    let errorValues = [];
+    for (let i = 0; i < input[1].length; i++) {
+        if (isNaN(input[1][i])) {
+            errorFields.push(input[0][i]);
+            errorValues.push(input[1][i]);
+        }
+    }
+    if (errorFields.length !== 0) {
+        totalAmount = `Параметр ${errorFields} содержит неправильное значение ${errorValues}`
+    } else {
+        let today = new Date;
+        today = today.getFullYear() * 12 + today.getMonth();
+        date = date.getFullYear() * 12 + date.getMonth();
+        let p = percent / 100 / 12;
+        let months = Math.abs(date - today);
+        let body = amount - contribution;
+        let monthlyPayment;
+        monthlyPayment = body * (p + p / ((Math.pow((1 + p), months)) - 1));
+        totalAmount = parseFloat((monthlyPayment * months).toFixed(2));
+        console.log(monthlyPayment);
+    }
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (!name) {
+        name = 'Аноним'
+    }
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    return greeting;
 }
