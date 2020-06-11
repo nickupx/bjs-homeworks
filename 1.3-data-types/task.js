ac13b0ad1c77a9f0326db6f929763d9330936695
nickupx <nick.upsky@gmail.com>
2020-06-12 00:53:22 +0300

    added past date check

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -21,15 +21,19 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
         totalAmount = `Параметр ${errorFields} содержит неправильное значение ${errorValues}`
     } else {
         let today = new Date;
-        today = today.getFullYear() * 12 + today.getMonth();
-        date = date.getFullYear() * 12 + date.getMonth();
-        let p = percent / 100 / 12;
-        let months = Math.abs(date - today);
-        let body = amount - contribution;
-        let monthlyPayment;
-        monthlyPayment = body * (p + p / ((Math.pow((1 + p), months)) - 1));
-        totalAmount = parseFloat((monthlyPayment * months).toFixed(2));
-        console.log(monthlyPayment);
+        if (date <= today) {
+            totalAmount = `Марти Макфлай в ипотеке не нуждается`
+        } else {
+            today = today.getFullYear() * 12 + today.getMonth();
+            date = date.getFullYear() * 12 + date.getMonth();
+            let p = percent / 100 / 12;
+            let months = date - today;
+            let body = amount - contribution;
+            let monthlyPayment;
+            monthlyPayment = body * (p + p / ((Math.pow((1 + p), months)) - 1));
+            totalAmount = parseFloat((monthlyPayment * months).toFixed(2));
+            console.log(monthlyPayment);
+        }
     }
 
     return totalAmount;
