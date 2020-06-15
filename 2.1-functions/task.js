a78e66af66cbd7347f3d0de26b2e9f72e48b04e5
nickupx <nick.upsky@gmail.com>
2020-06-15 23:49:30 +0300

    solved after 3 hours

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,18 +1,17 @@
 function getSolutions(a, b, c) {
-    let solutions;
-    let x1, x2;
-    let d = (b * b) - (4 * a * c);
-    if (d < 0) {
-        solutions = { D: d, roots: [] }
+    let solutions, roots, x1, x2;
+    let D = (b * b) - (4 * a * c);
+    if (D < 0) {
+        solutions = { D, roots: [] }
     }
-    if (d == 0) {
+    if (D == 0) {
         x1 = -b / 2 * a;
-        solutions = { D: d, roots: [x1] }
+        solutions = { D, roots: [x1] }
     }
-    if (d > 0) {
-        x1 = (-b + Math.sqrt(d)) / (2 * a);
-        x2 = (-b - Math.sqrt(d)) / (2 * a);
-        solutions = { D: d, roots: [x1, x2] }
+    if (D > 0) {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+        solutions = { D, roots: [x1, x2] }
     }
     return solutions;
 }
@@ -32,11 +31,66 @@ function showSolutionsMessage(a, b, c) {
     }
 }
 
+data = {
+    algebra: [5, 3, 4, 1],
+    geometry: [2, 4, 5],
+    russian: [4, 5],
+    physics: [4, 5],
+    music: [4, 5],
+    english: [4, 5],
+    poetry: [4, 5],
+    chemistry: [4, 1, 3],
+    french: [4, 5, 1],
+}
+
+data2 = {};
+
 function getAverageScore(data) {
+    let avgData = {};
+    if (Object.keys(data).length > 0) {
+        for (let key in data) {
+            avgData[key] = getAverageMark(data[key]);
+        }
+        let sumAvg = 0;
 
+        for (let key in avgData) {
+            sumAvg += avgData[key];
+        }
+        avgData.average = sumAvg / (Object.keys(avgData).length);
 
+    } else {
+        avgData.average = 0;
+    }
+    return avgData;
 }
 
 function getAverageMark(marks) {
+    let sumMark = 0;
+    for (i = 0; i < marks.length; i++) {
+        sumMark += marks[i];
+    }
+    if (sumMark > 0) {
+        return sumMark / marks.length
+    } else { return sumMark }
+}
 
+secretData = {
+    aaa: 1,
+    bbb: 0,
+}
+
+function getPersonData(secretData) {
+    let personData = { firstName: getDecodedValue(secretData.aaa), lastName: getDecodedValue(secretData.bbb) };
+    return personData;
+}
+
+function getDecodedValue(secret) {
+    let value;
+    if (secret == 0) {
+        value = 'Родриго'
+    }
+    if (secret == 1) {
+        value = 'Эмильо'
+    }
+    return value;
 }
