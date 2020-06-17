211c598de1baac7d10e7f2b425ec34cff756c212
nickupx <nick.upsky@gmail.com>
2020-06-17 20:26:52 +0300

    2-3 solved

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,37 @@
-//String.prototype.isPalindrome - для задачи №1
+// String.prototype.isPalindrome;
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+function isPalindrome(str) {
+    const origin = str.toLowerCase().replace(/\s+/g, '');
+    const reversed = origin.split('').reverse().join('');
+    if (reversed == origin) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+function getAverageMark(marks) {
+    if (marks.length == 0) {
+        return 0;
+    } else {
+        let sumMark = 0;
+        for (let i = 0; i < marks.length; i++) {
+            sumMark += marks[i];
+        }
+        const average = sumMark / marks.length;
+        const roundedAverage = Math.round(average);
+        return roundedAverage;
+    }
+}
+
+function checkBirthday(bday) {
+    const now = +new Date();
+    const birthday = +new Date(bday);
+    const diff = now - birthday;
+    const age = diff / (365 * 24 * 60 * 60 * 1000);
+    if (age >= 18) {
+        return true
+    } else {
+        return false
+    }
 }
